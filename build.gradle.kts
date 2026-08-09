dbe4d5ef19553d5e392464fb0f047eb185194b74
Firedrago95 <yonghwa95@naver.com>
2026-08-09 14:20:48 +0900

    chore: 프로젝트 초기 설정

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "cache.lite"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
